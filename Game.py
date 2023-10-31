29be270af74043a2a04b3360b5961463379c25b8
Kamil Dominko <kamil.dominko@gmail.com>
2023-10-31 08:03:53 +0100

    Przed refaktoryzacją wczytywania obrazków.

--- Game.py
+++ Game.py
@@ -40,7 +40,6 @@ class Game:
 
         self.ground_y = self.screen.get_height() - self.ground_img.get_height()
 
-
         self.player = Player(self)
 
         # Groups
@@ -50,10 +49,13 @@ class Game:
         self.obstacle_group = pygame.sprite.Group()
 
         # Intro screen
-        self.player_stand = pygame.image.load(
+        self.start_img = pygame.image.load(
             'graphics/player/sprite_knight_stand.png').convert_alpha()
-        self.player_stand = pygame.transform.rotozoom(self.player_stand, 0, 2)
-        self.player_stand_rect = self.player_stand.get_rect(center=(400, 200))
+        self.start_img = useful.scale_image(self.start_img,
+                                            self.scale_x, self.scale_y)
+        self.start_img = pygame.transform.rotozoom(self.start_img, 0, 2)
+        self.start_img_rect = self.start_img.get_rect(
+            center=self.screen.get_rect().center)
 
         self.title_msg = self.font.render(self.settings.game_title, False,
                                           self.settings.font_color)
@@ -69,7 +71,7 @@ class Game:
 
     def _display_start_screen(self):
         self.screen.fill(self.settings.theme_color)
-        self.screen.blit(self.player_stand, self.player_stand_rect)
+        self.screen.blit(self.start_img, self.start_img_rect)
         self.screen.blit(self.title_msg, self.title_msg_rect)
         if self.score == 0:
             self.screen.blit(self.start_msg, self.start_msg_rect)
@@ -81,26 +83,24 @@ class Game:
 
     def display_score(self):
         current_time = int(pygame.time.get_ticks() / 1000) - self.start_time
-        score_surf = self.font.render(f'Score: {current_time}', False,
+        score_surf = self.font.render(f'Score: {current_time}', True,
                                       self.settings.BLACK)
-        score_rect = score_surf.get_rect(center=(400, 50))
+        score_rect = score_surf.get_rect(midtop=(self.screen_w / 2, 0))
+        print(score_rect.midtop)
         self.screen.blit(score_surf, score_rect)
         return current_time
 
     def _check_collision(self):
         if pygame.sprite.spritecollide(self.players.sprite, self.obstacle_group,
                                        True):
-            # self.obstacle_group.empty()
-            # self.game_active = False
             self.player.current_health -= self.settings.damage
             if self.player.current_health <= 0:
                 self.game_active = False
 
     def _update_screen(self):
         self.screen.blit(self.bg_img, (0, 0))
-        # self.screen.blit(self.ground_img, (0, 300 * self.scale_x))
-
         self.screen.blit(self.ground_img, (0, self.ground_y))
+
         self.score = self.display_score()
 
         self.players.draw(self.screen)
@@ -116,11 +116,16 @@ class Game:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.isRunning = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    self.isRunning = False
 
             if self.game_active:
                 if event.type == self.obstacle_timer:
                     self.obstacle_group.add(Obstacle(self,
-                        choice(['storm', 'snail', 'snail', 'snail'])))
+                                                     choice(['storm', 'snail',
+                                                             'snail',
+                                                             'snail'])))
 
             else:
                 if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
--- useful.py
+++ useful.py
@@ -9,7 +9,18 @@ def get_scale(screen: pygame.surface.Surface, image: pygame.surface.Surface):
 
 
 def scale_image(image: pygame.surface.Surface, scale_x, scale_y):
+    """Funkcja skaluje podany obraz image przez wartości skali scale_x i
+    scale_y."""
     _image = pygame.transform.scale(image,
                                     (image.get_width() * scale_x,
                                      image.get_height() * scale_y))
     return _image
+
+
+def load_scale_image(path, img_name, scale_x, scale_y):
+    """Funkcja wczytuje z podanej ścierzki path obraz img_name i skaluje go
+    przez podane skale scale_x i scale_y."""
+    path = path
+    img = pygame.image.load(f"{path}/{img_name}")
+    img = scale_image(img, scale_x, scale_y)
+    return img
